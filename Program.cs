ecffbd79d54c0363a20d3024fd3e84a1bc883ecf
Bárbara <babyr.oliveira@gmail.com>
2020-03-20 14:19:02 -0300

    Resolução do exeríicio proposto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, digite uma frase.");
+            string frase = Console.ReadLine().Replace("r" , "l");
+            Console.WriteLine(frase);
+        }
+    }
+}
